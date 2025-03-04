b8c91a9ce772377f01037e12050af4bf0743422a
Ricardo Luis Barbosa Pereira <patresio@gmail.com>
2025-03-04 10:51:40 -0300

    components template flex and grid

---
+++ src/components/template/Flex.tsx
@@ -0,0 +1,19 @@
+import mergeClasses from "@/utils/mergeClasses";
+
+interface FlexProps {
+  children: React.ReactNode;
+  className?: string;
+  col?: boolean;
+  row?: boolean;
+}
+
+const Flex = ({ children, className, col, row }: FlexProps) => {
+  return ( 
+    <div className={mergeClasses("flex justify-center items-center gap-2", {'flex-col': col, 'flex-row': row}, className)}>
+      {children}
+    </div>
+
+   );
+}
+ 
+export default Flex;
---
+++ src/utils/mergeClasses.ts
@@ -0,0 +1,6 @@
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export default function mergeClasses(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
