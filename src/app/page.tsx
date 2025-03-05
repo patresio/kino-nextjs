14b01d8147ec7976afb5e865044eed1578455802
Ricardo Luis Barbosa Pereira <patresio@gmail.com>
2025-03-04 22:58:25 -0300

    install phosphor react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import Carrousel from "@/components/template/Carrousel";
 
 
 export default function Home() {
   return (
+    <Carrousel>
+      <div className="w-full h-[500px] bg-red-500">1</div>
+      <div className="w-full h-[500px] bg-red-500">2</div>
+      <div className="w-full h-[500px] bg-red-500">3</div>
+      <div className="w-full h-[500px] bg-red-500">4</div>
+    </Carrousel>
   );
 }
