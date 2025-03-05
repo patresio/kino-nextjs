12bda2cf69ae31d2088f1a851d78fc61e4e6049c
Ricardo Luis Barbosa Pereira <patresio@gmail.com>
2025-03-05 11:01:09 -0300

    changing icon and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import Carrousel from "@/components/template/Carrousel";
-
+import Carrousel from '@/components/template/Carrousel'
 
 export default function Home() {
   return (
     <Carrousel>
-      <div className="w-full h-[500px] bg-red-500">1</div>
-      <div className="w-full h-[500px] bg-red-500">2</div>
-      <div className="w-full h-[500px] bg-red-500">3</div>
-      <div className="w-full h-[500px] bg-red-500">4</div>
+      <div className="w-20 h-20 p-8 bg-red-500">1</div>
+      <div className="w-20 h-20 p-8 bg-yellow-500">2</div>
+      <div className="w-20 h-20 p-8 bg-blue-500">3</div>
+      <div className="w-20 h-20 p-8 bg-green-500">4</div>
     </Carrousel>
-  );
+  )
 }
